cf66145b1b1910839fea22bedb00a0e8b0ceb63c
HudsonSchumaker <hudson.schumaker@me.com>
2022-05-07 20:54:53 +0200

    ST-HS-OSX deck

--- src/Common.hpp
+++ src/Common.hpp
@@ -6,11 +6,15 @@
 
 #pragma once
 #include <map>
+#include <array>        // std::array
 #include <math.h>
 #include <vector>
+#include <random>       // std::default_random_engine
+#include <chrono> 
 #include <string>
 #include <stdlib.h>
 #include <iostream>
+#include <algorithm>    // std::shuffle
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
