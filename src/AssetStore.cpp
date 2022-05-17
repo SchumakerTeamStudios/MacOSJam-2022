3dbc602af0c336d2129707488711e47f1448c4df
HudsonSchumaker <hudson.schumaker@me.com>
2022-05-17 22:17:06 +0200

    Aseet

--- src/AssetStore.cpp
+++ src/AssetStore.cpp
@@ -18,21 +18,22 @@ AssetStore* AssetStore::getInstance() {
     return instance;
 }
 
-void AssetStore::addTexture(SDL_Renderer* renderer, const short assetId, const std::string& filePath) {
+void AssetStore::addTexture(SDL_Renderer* renderer, const short assetId, const short value, const std::string& filePath) {
     SDL_Surface* surface = IMG_Load(filePath.c_str());
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_FreeSurface(surface);
 
-    textures.emplace(assetId, texture);
+    std::pair temp = { value, texture };
+    textures.emplace(assetId, temp);
 }
 
 SDL_Texture* AssetStore::getTexture(const short assetId) {
-    return textures[assetId];
+    return textures[assetId].second;
 }
 
 void AssetStore::clearAssets() {
     for (auto texture: textures) {
-        SDL_DestroyTexture(texture.second);
+        SDL_DestroyTexture(texture.second.second);
     }
     textures.clear();
 }
