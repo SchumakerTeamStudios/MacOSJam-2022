916aacba035a6565863eff35b89c8acad38ddb44
HudsonSchumaker <hudson.schumaker@me.com>
2022-05-17 21:59:12 +0200

    header

--- src/Common.hpp
+++ src/Common.hpp
@@ -6,16 +6,17 @@
 
 #pragma once
 #include <map>
-#include <array>        // std::array
+#include <array>     // std::array
 #include <math.h>
 #include <vector>
-#include <random>       // std::default_random_engine
+#include <random>    // std::default_random_engine
 #include <chrono> 
 #include <future>
 #include <string>
+#include <utility>   // std:pair<T,T>
 #include <stdlib.h>
 #include <iostream>
-#include <algorithm>    // std::shuffle
+#include <algorithm> // std::shuffle
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
